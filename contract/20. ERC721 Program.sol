f4f264de29177e0fc06a0e6cde493a5b52e6d3ac
abhishekrapid <abhisheknegi@Abhisheks-MacBook-Air.local>
2022-07-03 10:51:14 +0530

    add: erc721 contract sample program

--- contract/20. ERC721 Program.sol
+++ contract/20. ERC721 Program.sol
@@ -1,39 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity 0.8.0;
 
-import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/tokens/nf-token-metadata.sol";
+import "https://github.com/0xcert/ethereum-erc721/src/contracts/ownership/ownable.sol";
 
-contract CoolBitcoin is ERC721, ERC721Enumerable, Ownable {
-    using Counters for Counters.Counter;
+contract newNFT is NFTokenMetadata, Ownable {
 
-    Counters.Counter private _tokenIdCounter;
+  constructor() {
+    nftName = "CoolBitcoin";
+    nftSymbol = "CBTC";
+  }
 
-    constructor() ERC721("CoolBitcoin", "CBTC") {}
+  function mint(address _to, uint256 _tokenId, string calldata _uri) external onlyOwner {
+    super._mint(_to, _tokenId);
+    super._setTokenUri(_tokenId, _uri);
+  }
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-    }
-
-    // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
 }
